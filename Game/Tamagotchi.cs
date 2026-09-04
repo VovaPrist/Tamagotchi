f9e0bb173d0a31a179f8b1ec305e77609030f022
Vova <vovaprist089@gmail.com>
2026-09-04 12:20:18 +0200

    initial commit

---
+++ Game/Tamagotchi.cs
@@ -0,0 +1,47 @@
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List<String> words;
+    private bool isAlive;
+    public string Name;
+
+    public void Feed()
+    {
+        
+    }
+
+    public void Hi()
+    {
+        
+    }
+
+    public void Teach(string word)
+    {
+        
+    }
+
+    public void Tick()
+    {
+        
+    }
+
+    public void PrintStats()
+    {
+        
+    }
+
+    public bool GetAlive()
+    {
+        return true;
+    }
+
+    private void ReduceBoredom()
+    {
+        
+    }
+
+}
